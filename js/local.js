882f703ad6a3c5620aeab672bb4aec039a33ae5f
martin <telemagico@gmail.com>
2017-03-31 20:27:45 -0300

    fb driver approach

--- js/local.js
+++ js/local.js
@@ -1,9 +1,10 @@
 $(function(){
 	var key = helpers.getParameterByName('key')
-	, a = key ? 'locales/' + key : null
+	, edit = key ? 'locales/' + key : null
 
-	
-	helpers.render_row(a, '.locales--container', '#locales', function(){
-		$('#loading').fadeOut(200)	
+	helpers.firebase_once('categorias', function(categorias){ 
+		helpers.render_row(edit,'.locales--container', '#locales', {categorias: categorias}, function(){
+			$('#loading').fadeOut(200)	
+		})
 	})
 })
