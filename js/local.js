2522e455d5ea3efca9a4a592701306b24cde96f8
martin <telemagico@gmail.com>
2017-03-31 11:37:14 -0300

    update hk

--- js/local.js
+++ js/local.js
@@ -1,6 +1,8 @@
 $(function(){
 	var key = helpers.getParameterByName('key')
 	, a = key ? 'locales/' + key : null
+
+	
 	helpers.render_row(a, '.locales--container', '#locales', function(){
 		$('#loading').fadeOut(200)	
 	})
