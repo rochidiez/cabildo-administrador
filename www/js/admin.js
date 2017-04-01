174a21740fdfdd300d114effd990ffc7a397f850
martin <telemagico@gmail.com>
2017-04-01 10:52:58 -0300

    heroku forced www folder

--- www/js/admin.js
+++ www/js/admin.js
@@ -1,24 +1,24 @@
 $(function(){
 
-	// bring all and sort.
-	var ref = firebase.database().ref('locales')
-	ref.once('value').then(function(snap) {
-
-		var values = snap.val()
-		, tabs = {"todos":[],"premium":[],"basico":[],"avenida":[]}
-
-		for(var i in values){
-			var row = values[i]
-    		tabs.todos.push(row)
-    		if(row.plan){
-    			tabs[row.plan].push(row)
-    		}
-    	}
-
-		$('div[data-w-tab="todos"] .locales').html($.templates('#local').render(tabs.todos))
-		$('div[data-w-tab="premium"] .locales').html($.templates('#local').render(tabs.premium))
-		$('div[data-w-tab="basico"] .locales').html($.templates('#local').render(tabs.basico))
-		$('div[data-w-tab="avenida"] .locales').html($.templates('#local').render(tabs.avenida))
-
+	$(document).on('click','.edit.table-action',function(){
+		location.href = 'local.html?key=' + $(this).data('key')
 	})
+
+	$(document).on('click','.eliminar.table-action',function(){
+		$('body').attr('data-key',$(this).data('key'))
+	})	
+
+	// modal action
+
+	// bring all and sort.
+	helpers.render_tabs('locales','#locales',
+		{
+			"todos":[]
+			,"premium":[]
+			,"basico":[]
+			,"avenida":[]
+		}, function(){
+			$('#loading').fadeOut(200)	
+		}
+	)
 })
--- www/js/local.js
+++ www/js/local.js
@@ -1,3 +1,10 @@
 $(function(){
+	var key = helpers.getParameterByName('key')
+	, edit = key ? 'locales/' + key : null
 
+	helpers.firebase_once('categorias', function(categorias){ 
+		helpers.render_row(edit,'.locales--container', '#locales', {categorias: categorias}, function(){
+			$('#loading').fadeOut(200)	
+		})
+	})
 })
